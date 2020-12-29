066aaaf892cf2cae24b4ecaee42e7e45f1a8e44e
Marcus Scese <43016531+mdscese@users.noreply.github.com>
2020-12-29 00:23:40 -0500

    Add files via upload

---
+++ scenes/scenecreator.c
@@ -0,0 +1,4 @@
+// TO DO: implement a way to create scene files
+// TO DO: implement a way to use a debuging mode an make objects in a scene that way
+// TO DO: implement a way to import models into a scene
+// TO DO: implement a way to compress the files
